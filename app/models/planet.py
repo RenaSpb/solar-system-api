a38191c87fb3ca493db6fa08632789e15a6781d9
Renata <murzinarenata@gmail.com>
2025-04-17 11:36:43 -0700

    wave 01

---
+++ app/models/planet.py
@@ -0,0 +1,16 @@
+class Planet:
+    def __init__(self, id, name, description, size):
+        self.id = id
+        self.name = name
+        self.description = description
+        self.size = size
+
+
+planets = [
+    Planet(1, "Flora", "Beautiful", "big"),
+    Planet(2, "Renata", "Amazing", "big"),
+    Planet(3, "Kira", "Fantastic", "medium"),
+    Planet(4, "Darina", "Kind", "small")
+]
+
+
