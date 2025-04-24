df3bed749dfded51204a0b88a4c1bab196de0c70
Renata <murzinarenata@gmail.com>
2025-04-24 11:39:53 -0700

    wave 3

--- app/models/planet.py
+++ app/models/planet.py
@@ -1,23 +1,32 @@
-class Planet:
-    def __init__(self, id, name, description, size):
-        self.id = id
-        self.name = name
-        self.description = description
-        self.size = size
+# class Planet:
+#     def __init__(self, id, name, description, size):
+#         self.id = id
+#         self.name = name
+#         self.description = description
+#         self.size = size
     
-    def to_dict(self):
-        return {
-            "id": self.id,
-            "name": self.name,
-            "description": self.description,
-            "size": self.size
-        }
+#     def to_dict(self):
+#         return {
+#             "id": self.id,
+#             "name": self.name,
+#             "description": self.description,
+#             "size": self.size
+#         }
 
-planets = [
-    Planet(1, "Flora", "Beautiful", "big"),
-    Planet(2, "Renata", "Amazing", "big"),
-    Planet(3, "Kira", "Fantastic", "medium"),
-    Planet(4, "Darina", "Kind", "small")
-]
+# planets = [
+#     Planet(1, "Flora", "Beautiful", "big"),
+#     Planet(2, "Renata", "Amazing", "big"),
+#     Planet(3, "Kira", "Fantastic", "medium"),
+#     Planet(4, "Darina", "Kind", "small")
+# ]
+
+from sqlalchemy.orm import Mapped, mapped_column
+from ..db import db
+
+class Planet(db.Model):
+    id: Mapped[int] = mapped_column(primary_key=True, autoincrement=True)
+    name: Mapped[str]
+    description: Mapped[str]
+    size: Mapped[str]
 
 
--- app/routes/planets_routes.py
+++ app/routes/planets_routes.py
@@ -1,34 +1,73 @@
-from flask import Blueprint, abort, make_response
-from app.models.planet import planets
+from flask import Blueprint, abort, make_response, request
+from app.models.planet import Planet
+from ..db import db
 
 planets_bp = Blueprint("planets_bp", __name__, url_prefix="/planets")
 
+# @planets_bp.get("")
+# def get_all_planets():
+#     planets_response = [planet.to_dict() for planet in planets]
+#     return planets_response
+
+# @planets_bp.get("/<planet_id>")
+# def get_one_planet(planet_id):
+#     planet = validate_planet(planet_id)
+
+#     return {
+#         "id": planet.id,
+#         "name": planet.name,
+#         "description": planet.description,
+#         "size" : planet.size
+#     }, 200
+
+# def validate_planet(planet_id):
+#     try:
+#         planet_id = int(planet_id)
+#     except:
+#         response = {"message": f"planet {planet_id} invalid"}
+#         abort(make_response(response, 400))
+        
+#     for planet in planets:
+#         if planet.id == planet_id:
+#             return planet
+        
+#     response = {"message": f"planet {planet_id} not found"}
+#     abort(make_response(response, 404))
+
+@planets_bp.post("")
+def create_planet():
+    request_body = request.get_json()
+    name = request_body["name"]
+    description = request_body["description"]
+    size = request_body["size"]
+
+    new_planet = Planet(name=name, description=description, size=size)
+    db.session.add(new_planet)
+    db.session.commit()
+
+    response = {
+        "id": new_planet.id,
+        "name": new_planet.name,
+        "description": new_planet.description,
+        "size": new_planet.size,
+    }
+    return response, 201
+
 @planets_bp.get("")
 def get_all_planets():
-    planets_response = [planet.to_dict() for planet in planets]
-    return planets_response
+    query = db.select(Planet).order_by(Planet.id)
+    planets = db.session.scalars(query)
+    # We could also write the line above as:
+    # books = db.session.execute(query).scalars()
 
-@planets_bp.get("/<planet_id>")
-def get_one_planet(planet_id):
-    planet = validate_planet(planet_id)
-
-    return {
-        "id": planet.id,
-        "name": planet.name,
-        "description": planet.description,
-        "size" : planet.size
-    }, 200
-
-def validate_planet(planet_id):
-    try:
-        planet_id = int(planet_id)
-    except:
-        response = {"message": f"planet {planet_id} invalid"}
-        abort(make_response(response, 400))
-        
+    planets_response = []
     for planet in planets:
-        if planet.id == planet_id:
-            return planet
-        
-    response = {"message": f"planet {planet_id} not found"}
-    abort(make_response(response, 404))
+        planets_response.append(
+            {
+                "id": planet.id,
+                "name": planet.name,
+                "description": planet.description,
+                "size": planet.size,
+            }
+        )
+    return planets_response
