09506914410462a426f4df043553060a0ad5e49c
Renata <murzinarenata@gmail.com>
2025-04-28 10:49:12 -0700

    Wave 4

--- app/routes/planets_routes.py
+++ app/routes/planets_routes.py
@@ -1,38 +1,20 @@
-from flask import Blueprint, abort, make_response, request
+from flask import Blueprint, abort, make_response, request, Response
 from app.models.planet import Planet
 from ..db import db
 
 planets_bp = Blueprint("planets_bp", __name__, url_prefix="/planets")
 
-# @planets_bp.get("")
-# def get_all_planets():
-#     planets_response = [planet.to_dict() for planet in planets]
-#     return planets_response
 
-# @planets_bp.get("/<planet_id>")
-# def get_one_planet(planet_id):
-#     planet = validate_planet(planet_id)
+@planets_bp.get("/<planet_id>")
+def get_one_planet(planet_id):
+    planet = validate_planet(planet_id)
 
-#     return {
-#         "id": planet.id,
-#         "name": planet.name,
-#         "description": planet.description,
-#         "size" : planet.size
-#     }, 200
-
-# def validate_planet(planet_id):
-#     try:
-#         planet_id = int(planet_id)
-#     except:
-#         response = {"message": f"planet {planet_id} invalid"}
-#         abort(make_response(response, 400))
-        
-#     for planet in planets:
-#         if planet.id == planet_id:
-#             return planet
-        
-#     response = {"message": f"planet {planet_id} not found"}
-#     abort(make_response(response, 404))
+    return {
+        "id": planet.id,
+        "name": planet.name,
+        "description": planet.description,
+        "size" : planet.size
+    }, 200
 
 @planets_bp.post("")
 def create_planet():
@@ -57,8 +39,6 @@ def create_planet():
 def get_all_planets():
     query = db.select(Planet).order_by(Planet.id)
     planets = db.session.scalars(query)
-    # We could also write the line above as:
-    # books = db.session.execute(query).scalars()
 
     planets_response = []
     for planet in planets:
@@ -70,4 +50,43 @@ def get_all_planets():
                 "size": planet.size,
             }
         )
+
     return planets_response
+
+@planets_bp.put("/<planet_id>")
+def update_planet(planet_id):
+    planet = validate_planet(planet_id)
+    request_body = request.get_json()
+
+    planet.name = request_body["name"]
+    planet.size = request_body["size"]
+    planet.description = request_body["description"]
+    db.session.commit()
+
+    return Response(status=204, mimetype="application/json")
+
+@planets_bp.delete("/<planet_id>")
+def delete_book(planet_id):
+    planet = validate_planet(planet_id)
+    db.session.delete(planet)
+    db.session.commit()
+
+    return Response(status=204, mimetype="application/json")
+
+
+def validate_planet(planet_id):
+    try:
+        planet_id = int(planet_id)
+    except:
+        response = {"message": f"planet {planet_id} invalid"}
+        abort(make_response(response, 400))
+        
+    query = db.select(Planet).where(Planet.id == planet_id)
+    planet = db.session.scalar(query)
+
+    if not planet:
+        response = {"message": f"planet {planet_id} not found"}
+        abort(make_response(response, 404))
+
+    return planet
+   
